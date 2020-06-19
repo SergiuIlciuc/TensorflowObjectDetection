cb9770ef3d81363b9687fc49dda8ada0b48054db
Sergiu Ilciuc <ilciuc.sergiu@assist.ro>
2020-06-19 11:25:19 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,105 @@
+import { Component, OnInit } from '@angular/core';
+
+//import COCO-SSD model as cocoSSD
+import * as cocoSSD from '@tensorflow-models/coco-ssd';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent implements OnInit
+{
+  title = 'TF-ObjectDetection';
+  private video: HTMLVideoElement;
+  public firstDraw = false;
+
+
+  ngOnInit()
+  {
+    this.webcam_init();
+    this.predictWithCocoModel();
+  }
+
+public async predictWithCocoModel(){
+  const model = await cocoSSD.load({base:'lite_mobilenet_v2'});
+  this.detectFrame(this.video,model);
+  console.log('model loaded');
+}
+
+webcam_init()
+  {
+  this.video = <HTMLVideoElement> document.getElementById("vid");
+
+     navigator.mediaDevices
+    .getUserMedia({
+    audio: false,
+    video: {
+      facingMode: "user",
+    }
+     })
+    .then(stream => {
+    this.video.srcObject = stream;
+    this.video.onloadedmetadata = () => {
+      this.video.play();
+    };
+    });
+  }
+
+  detectFrame = (video, model) => {
+    model.detect(video).then(predictions => {
+      this.renderPredictions(predictions);
+      requestAnimationFrame(() => {
+        this.detectFrame(video, model);
+      });
+    });
+  }
+
+  renderPredictions = predictions => {
+    this.firstDraw= true;
+
+    const canvas = <HTMLCanvasElement> document.getElementById("canvas");
+
+    const ctx = canvas.getContext("2d");
+
+    canvas.width  = 600;
+    canvas.height = 600;
+
+const rateW = canvas.width/300;
+const rateH = canvas.height/300;
+
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    // Font options.
+    const font = "16px sans-serif";
+    ctx.font = font;
+    ctx.textBaseline = "top";
+    ctx.drawImage(this.video,0, 0,canvas.width,canvas.height);
+
+    predictions.forEach(prediction => {
+      const x = prediction.bbox[0]*rateW;
+      const y = prediction.bbox[1]*rateH;
+      const width = prediction.bbox[2]*rateW;
+      const height = prediction.bbox[3]*rateH;
+      // Draw the bounding box.
+      ctx.strokeStyle = "#00FFFF";
+      ctx.lineWidth = 2;
+      ctx.strokeRect(x, y, width, height);
+      // Draw the label background.
+      ctx.fillStyle = "#00FFFF";
+      const textWidth = ctx.measureText(prediction.class).width;
+      const textHeight = parseInt(font, 10); // base 10
+      ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
+    });
+
+    predictions.forEach(prediction => {
+      const x = prediction.bbox[0]*rateW;
+      const y = prediction.bbox[1]*rateH;
+      // Draw the text last to ensure it's on top.
+      ctx.fillStyle = "#000000";
+      ctx.fillText(prediction.class, x, y);
+    });
+  };
+
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    MatProgressSpinnerModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
