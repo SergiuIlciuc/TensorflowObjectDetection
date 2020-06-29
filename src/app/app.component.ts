fd98e9c71b209002ad20a961ed60cc93ad1a424b
Sergiu Ilciuc <ilciuc.sergiu@assist.ro>
2020-06-29 21:08:17 +0300

    Added custom model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
- /* 3131B Grigoras Emanuel, Ciprian Iacob, Iriciuc Andrei, Ilciuc Sergiu */
+/* 3131B Grigoras Emanuel, Ciprian Iacob, Iriciuc Andrei, Ilciuc Sergiu */
 
 import { Component, OnInit } from '@angular/core';
+import { ObjectDetection } from './ObjectDetection';
 
 //import COCO-SSD model as cocoSSD
 import * as cocoSSD from '@tensorflow-models/coco-ssd';
@@ -14,23 +15,46 @@ import * as cocoSSD from '@tensorflow-models/coco-ssd';
 export class AppComponent implements OnInit {
   private video: HTMLVideoElement;
   public firstDraw = false;
+  public changedModel = false;
   public scoreThreshold = 0.9;
+  public modelType="Custom";
 
   public detectedObjects = [];
   public detectedObjectsSet = {};
 
   ngOnInit() {
-    this.webcam_init();
+    this.initialize_camera();
     this.predictWithCocoModel();
   }
 
-  public async predictWithCocoModel() {
-    const model = await cocoSSD.load({ base: 'lite_mobilenet_v2' });
+  loadModel(custom) {
+    this.modelType = custom? "Custom": "Official";
+    this.changedModel = true;
+
+    setTimeout(() => {
+      this.changedModel = false;
+      this.predictWithCocoModel(custom);
+    }, 100);
+  }
+
+  public async predictWithCocoModel(custom = true) {
+
+    let model = null;
+
+    if (custom) {
+      const x = new ObjectDetection('lite_mobilenet_v2');
+      const modelPromise = load();
+      model = await modelPromise;
+    }
+    else {
+      model = await cocoSSD.load({ base: 'lite_mobilenet_v2' });
+    }
+
     this.detectFrame(this.video, model);
     console.log('model loaded');
   }
 
-  webcam_init() {
+  initialize_camera() {
     this.video = <HTMLVideoElement>document.getElementById("vid");
 
     navigator.mediaDevices
@@ -49,6 +73,10 @@ export class AppComponent implements OnInit {
   }
 
   detectFrame = (video, model) => {
+    if (this.changedModel) {
+      return;
+    }
+
     model.detect(video).then(predictions => {
       this.renderPredictions(predictions);
       requestAnimationFrame(() => {
@@ -79,41 +107,45 @@ export class AppComponent implements OnInit {
 
 
     predictions.forEach(prediction => {
-      if(prediction.score> this.scoreThreshold){
-      const x = prediction.bbox[0] * rateW;
-      const y = prediction.bbox[1] * rateH;
-      const width = prediction.bbox[2] * rateW;
-      const height = prediction.bbox[3] * rateH;
+      if (prediction.score > this.scoreThreshold) {
+        const x = prediction.bbox[0] * rateW;
+        const y = prediction.bbox[1] * rateH;
+        const width = prediction.bbox[2] * rateW;
+        const height = prediction.bbox[3] * rateH;
 
-      // Draw the bounding box.
-      ctx.strokeStyle = "#00FFFF";
-      ctx.lineWidth = 2;
-      ctx.strokeRect(x, y, width, height);
+        // Draw the bounding box.
+        ctx.strokeStyle = "#00FFFF";
+        ctx.lineWidth = 2;
+        ctx.strokeRect(x, y, width, height);
 
-      // Draw the label background.
-      ctx.fillStyle = "#00FFFF";
-      const textWidth = ctx.measureText(prediction.class+'100%').width;
-      const textHeight = parseInt(font, 10); // base 10
-      ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
+        // Draw the label background.
+        ctx.fillStyle = "#00FFFF";
+        const textWidth = ctx.measureText(prediction.class + '100%').width;
+        const textHeight = parseInt(font, 10); // base 10
+        ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
       }
     });
 
     predictions.forEach(prediction => {
-      if(prediction.score> this.scoreThreshold){
-      const x = prediction.bbox[0] * rateW;
-      const y = prediction.bbox[1] * rateH;
+      if (prediction.score > this.scoreThreshold) {
+        const x = prediction.bbox[0] * rateW;
+        const y = prediction.bbox[1] * rateH;
 
-      // Draw the text last to ensure it's on top.
-      ctx.fillStyle = "#000000";
-      ctx.fillText(prediction.class+' '+(prediction.score*100).toFixed()+'%', x, y);
+        // Draw the text last to ensure it's on top.
+        ctx.fillStyle = "#000000";
+        ctx.fillText(prediction.class + ' ' + (prediction.score * 100).toFixed() + '%', x, y);
 
-      if (!this.detectedObjectsSet[prediction.class]) {
-        this.detectedObjectsSet[prediction.class] = prediction.class;
-        this.detectedObjects.push(prediction.class);
+        if (!this.detectedObjectsSet[prediction.class]) {
+          this.detectedObjectsSet[prediction.class] = prediction.class;
+          this.detectedObjects.push(prediction.class);
+        }
       }
-    }
     });
   };
+}
 
-
+export async function load() {
+  const objectDetection = new ObjectDetection('lite_mobilenet_v2');
+  await objectDetection.load();
+  return objectDetection;
 }
---
+++ src/app/classes.ts
@@ -0,0 +1,15 @@
+ /* 3131B Grigoras Emanuel, Ciprian Iacob, Iriciuc Andrei, Ilciuc Sergiu */
+
+export interface ObjectDetectionClass {
+  name: string;
+  id: number;
+  displayName: string;
+}
+
+export const CLASSES: { [key: string]: ObjectDetectionClass } = {
+  1: {
+    name: '1leu',
+    id: 1,
+    displayName: '1leu',
+  },
+};
